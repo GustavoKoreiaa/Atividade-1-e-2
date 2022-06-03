fa43ad9f07cf39422e4881bc88848ac337c78351
GustavoKoreiaa <106408467+GustavoKoreiaa@users.noreply.github.com>
2022-06-03 15:17:17 -0300

    Atividade 1 e 2
    Gustavo

---
+++ main.py
@@ -0,0 +1,7 @@
+a = float(input('Informe o valor do salario: ').replace(',','.'))
+
+b = float(input('Informe o valor do aumneto do salario: ').replace(',','.'))
+
+c = a * (b/100)
+d = a+b 
+print('O valor do salario vai ser de R$%.2f'%d)
